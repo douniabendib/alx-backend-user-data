af9feedaf68589593077ad225624bc7163c4bc56
douniabendib <bendib.dounia@gmail.com>
2024-04-11 18:24:43 -0700

    done

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+0. Regex-ing
+"""
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Arguments:
+    fields: a list of strings
+    redaction: a string
+    message: a string
+    separator: a string
+    function called filter_datum that returns the log message
+    """
+
