7530450cb560314d79b40c96998d4caeac6a98b1
douniabendib <bendib.dounia@gmail.com>
2024-04-16 18:51:45 -0700

    done

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic auth
 """
+import base64
 from .auth import Auth
 
 
@@ -9,4 +10,17 @@ class BasicAuth(Auth):
     """
     class BasicAuth that inherits from Auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header for a Basic
+        Authorization
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        token = authorization_header.split(" ")[-1]
+        return token
